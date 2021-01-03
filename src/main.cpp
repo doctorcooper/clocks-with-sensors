69814cd9e82f1b45a4b6a129177580e4dfe31eae
Dmitry Kupriyanov <kuban1408@gmail.com>
2021-01-03 18:07:54 +0400

    Added timer library. Optimised code. Added pressure value displaying.

--- src/main.cpp
+++ src/main.cpp
@@ -4,23 +4,24 @@
 #include <RTClib.h>
 #include <Adafruit_BME280.h>
 #include "digits.h"
+#include <GyverTimer.h>
 
-#define PERIOD 1000                 // Частота опроса времени
-#define SENSOR_PERIOD 60000         // Частота опроса датчиков(1 минута)
-#define BME_ADDRESS 0x76            // Адрес i2c модуля датчиков
-#define RESET_CLOCK 0               // Флаг для подстройки часов при прошивке
+#define CLOCK_PERIOD 1000               // Частота опроса времени
+#define SENSOR_PERIOD 60000             // Частота опроса датчиков(1 минута)
+#define BME_ADDRESS 0x76                // Адрес i2c модуля датчиков
+#define RESET_CLOCK 0                   // Флаг для подстройки часов при прошивке
 
-LiquidCrystal_I2C lcd(0x27, 20, 4); // Дисплей
-RTC_DS3231 rtc;                     // Часы
-Adafruit_BME280 bme;                // Датчик
+LiquidCrystal_I2C lcd(0x27, 20, 4);     // Дисплей
+RTC_DS3231 rtc;                         // Часы
+Adafruit_BME280 bme;                    // Датчик
 
-uint32_t timer = 0;                 // Таймер для времени
-uint32_t sensorTimer = 0;           // Таймер для датчиков
-DateTime now;                       // Время
-uint8_t seconds, minutes, hours;    // Переменные для хранения времени
-bool dotFlag;                       // Флаг для точек
+GTimer_ms clockTimer(CLOCK_PERIOD);     // Таймер для времени
+GTimer_ms sensorTimer(SENSOR_PERIOD);   // Таймер для датчиков
+DateTime now;                           // Время
+uint8_t seconds, minutes, hours;        // Переменные для хранения времени
+bool dotFlag;                           // Флаг для точек
 
-// Опрос модуля часов и сохранение в память
+// Опрос модуля часов и сохранение в RAM
 void readClock() {
   now = rtc.now();
   seconds = now.second();
@@ -31,60 +32,54 @@ void readClock() {
 // Опрос датчиков и вывод на дисплей
 void drawSensor() {
   bme.takeForcedMeasurement();
-  String temperature = String(bme.readTemperature(), 1);
-  String humidity = String(bme.readHumidity(), 1);
+  String temperature = String(bme.readTemperature(), 0);
+  String humidity = String(bme.readHumidity(), 0);
+  String pressure = String(bme.readPressure() * 0.00750062, 0);
 
   lcd.setCursor(0, 3);
   for (uint8_t i = 0; i < 22; i++) {
-    lcd.print(F(" "));              // Очистка нижней строки
+    lcd.print(F(" "));                  // Очистка нижней строки
   }
   
-  lcd.setCursor(0, 3);              // Вывод инфо с датчиков
+  lcd.setCursor(0, 3);                  // Вывод инфо с датчиков
   lcd.print(F("  "));
   lcd.print(temperature);
   lcd.print(F("\337C"));
-  lcd.print(F("   "));
+  lcd.print(F("  "));
   lcd.print(humidity);
   lcd.print(F("%"));
+  lcd.print(F("  "));
+  lcd.print(pressure);
+  lcd.print(F("mm"));
 }
  
 // Обработка часов по таймеру
 void clockTick() {
-  if (millis() - timer >= PERIOD) {
-    dotFlag = !dotFlag;
-    printDots(&lcd, dotFlag);
+  dotFlag = !dotFlag;
+  printDots(&lcd, dotFlag);
 
-    seconds++;
-    if (seconds > 59) {
-      seconds = 0;
-      minutes++;
-      if(minutes <= 59) {
-        drawClocks(&lcd, hours, minutes);
-      }
-    }
-    if (minutes > 59) {
-      readClock();
+  seconds++;
+  if (seconds > 59) {
+    seconds = 0;
+    minutes++;
+    if (minutes <= 59) {
       drawClocks(&lcd, hours, minutes);
-      if (hours > 23) {
-        hours = 0;
-      }
     }
-
-    do {
-      timer += PERIOD;
-      if (timer < PERIOD) break;  // переполнение uint32_t
-    } while (timer < millis() - PERIOD); // защита от пропуска шага
+  }
+  if (minutes > 59) {
+    readClock();
+    drawClocks(&lcd, hours, minutes);
+    if (hours > 23) {
+      hours = 0;
+    }
   }
 }
 
-// Обработка датчиков по таймеру
-void sensorsTick() {
-  if (millis() - sensorTimer >= SENSOR_PERIOD) {
-    drawSensor();
-    do {
-      sensorTimer += SENSOR_PERIOD;
-      if (sensorTimer < SENSOR_PERIOD) break;  // переполнение uint32_t
-    } while (sensorTimer < millis() - SENSOR_PERIOD); // защита от пропуска шага
+void manualAdjustClock() {
+  if (Serial.available() > 0) {
+    String date = Serial.readString();
+    rtc.adjust(DateTime(date.c_str()));
+    Serial.println(F("Ok"));
   }
 }
 
@@ -111,6 +106,7 @@ void setup() {
 }
 
 void loop() {
-  clockTick();
-  sensorsTick();
+  if (clockTimer.isReady()) clockTick();
+  if (sensorTimer.isReady()) drawSensor();
+  // manualAdjustClock();
 }
